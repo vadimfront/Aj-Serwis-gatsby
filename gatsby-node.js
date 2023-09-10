c9db68e8c4a23fcdce38de69a781bf22f23451bb
vadimfront <vadimzila94@gmail.com>
2023-09-10 16:23:07 +0300

    dep test 2

--- gatsby-node.js
+++ gatsby-node.js
@@ -16,41 +16,42 @@ exports.createPages = async ({ graphql, actions }) => {
     }
   `)
 
-  if (result.errors) {
-    throw result.errors
+  if (!result) {
+    throw new Error("result error")
   }
 
-  const languages = result.data.allWp.nodes[0].languages
+  const languages = await result.data.allWp.nodes[0].languages
 
-  languages.forEach(language => {
-    const slug = language.slug === "uk" ? "/" : `/${language.slug}/`
+  languages &&
+    languages.forEach(language => {
+      const slug = language.slug === "uk" ? "/" : `/${language.slug}/`
 
-    createSlice({
-      id: `header--${language.slug}`,
-      component: path.resolve(`src/components/Header/Header.tsx`),
-      alias: `header--${language.slug}`,
-      context: {
-        lang: `header_menu_${language.slug}`,
-      },
+      createSlice({
+        id: `header--${language.slug}`,
+        component: path.resolve(`src/components/Header/Header.tsx`),
+        alias: `header--${language.slug}`,
+        context: {
+          lang: `header_menu_${language.slug}`,
+        },
+      })
+
+      createPage({
+        path: slug,
+        component: path.resolve(`src/templates/home-page.tsx`),
+        context: {
+          code: language.code,
+          slug: `header_menu_${language.slug}`,
+        },
+        slices: {
+          header: `header--${language.slug}`,
+        },
+      })
+      createPage({
+        path: `${slug}contacts`,
+        component: path.resolve(`src/templates/contact-page.tsx`),
+        slices: {
+          header: `header-${language.slug}`,
+        },
+      })
     })
-
-    createPage({
-      path: slug,
-      component: path.resolve(`src/templates/home-page.tsx`),
-      context: {
-        code: language.code,
-        slug: `header_menu_${language.slug}`,
-      },
-      slices: {
-        header: `header--${language.slug}`,
-      },
-    })
-    // createPage({
-    //   path: `${slug}contacts`,
-    //   component: path.resolve(`src/templates/contact-page.tsx`),
-    //   slices: {
-    //     header: `header-${language.slug}`,
-    //   },
-    // })
-  })
 }
